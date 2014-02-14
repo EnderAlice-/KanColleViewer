dea430df7a06c50ab572b8e918df698e48d3afe6
fin_alice <ender_alice@yahoo.com>
2014-02-14 15:39:53 +0900

    次のLvへの残り経験値表示を変更 - レベルが限界に達しているとき(即ち、次まで0の場合)に∞と表記するように

---
+++ Grabacr07.KanColleViewer/Views/Converters/ExperiencePointConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Grabacr07.KanColleViewer.Views.Converters
+{
+	[ValueConversion(typeof(int), typeof(string))]
+	public class ExperiencePointConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return "";
+			}
+
+			if (value is int)
+			{
+				var intValue = (int)value;
+
+				if (intValue <= 0)
+				{
+					return "∞";
+				}
+			}
+
+			return value.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
