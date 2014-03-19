0b41bc08e73cb87495fe5a09c98ecebf0ff84118
fin_alice <ender_alice@yahoo.com>
2014-03-19 09:01:40 +0900

    既定のソート項目をレベルに設定

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/ShipCatalogWindowViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/ShipCatalogWindowViewModel.cs
@@ -100,6 +100,7 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 			this.IsOpenSettings = true;
 
 			this.SortWorker = new ShipCatalogSortWorker();
+			this.SortWorker.SetTarget(ShipCatalogSortTarget.Level);
 
 			this.ShipTypes = KanColleClient.Current.Master.ShipTypes
 				.Select(kvp => new ShipTypeViewModel(kvp.Value)
